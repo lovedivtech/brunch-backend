443fd2e55d7c54f64e838161c43b62cd9b061f42
LOVEPICX <vijaykumarghariwala@gmail.com>
2025-11-07 18:39:42 +0530

    allow webp in imagekit

--- middleware/multerMiddlware.js
+++ middleware/multerMiddlware.js
@@ -1,12 +1,21 @@
 import multer from "multer";
 import { memoryStorage } from "multer";
 const MAX_SIZE = 10 * 1024 * 1024; // 10MB
-const allowedTypes = ["image/jpeg", "image/png", "image/jpg", "image/svg+xml"];
+const allowedTypes = [
+  "image/jpeg",
+  "image/png",
+  "image/jpg",
+  "image/svg+xml",
+  "image/webp",
+];
 const fileFilter = (req, file, cb) => {
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error("Only JPG, JPEG, PNG, and SVG files are allowed"), false);
+    cb(
+      new Error("Only JPG, JPEG, PNG, WEBP, and SVG files are allowed"),
+      false
+    );
   }
 };
 const upload = multer({
